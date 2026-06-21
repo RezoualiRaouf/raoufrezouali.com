1ad5b613a2d7995877302f1f3ce411ce002aaa03
raoufrezouali <127851417+RezoualiRaouf@users.noreply.github.com>
2026-06-21 12:32:30 +0100

    Add :GA4 QR code

--- src/app/qr/route.js
+++ src/app/qr/route.js
@@ -1,8 +1,10 @@
-// app/qr/route.js
 export async function GET(req) {
   const ip = req.headers.get('x-forwarded-for');
   const ua = req.headers.get('user-agent');
-  // log to DB, Vercel KV, or even append to a JSON file
   console.log({ time: new Date(), ip, ua });
-  return Response.redirect('https://raoufrezouali.com', 302);
+
+  return Response.redirect(
+    'https://raoufrezouali.com/?utm_source=business_card&utm_medium=qr_code&utm_campaign=2026',
+    302
+  );
 }
